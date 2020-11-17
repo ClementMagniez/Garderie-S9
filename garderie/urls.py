ba8d854a5ebca16741a91b85ce55115737fbe540
Clément <cl.magniez@laposte.net>
2020-11-17 23:02:23 +0100

    Ajout d'une redirection à la connexion
    + ajout de admin_index qui liste les tuples parent, enfants

--- garderie/urls.py
+++ garderie/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [ 
         path('index/', views.ListChildrenView.as_view(), name='index'),
-        path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+        path('enfant/<int:pk>/', views.ChildProfileView.as_view(), name='child_profile'),
+        path('parent/<int:pk>/', views.ParentProfileView.as_view(), name='parent_profile'),
+        path('index-redirect/', views.IndexRedirectView.as_view(), name='index_redirect'),
+        path('admin/', views.AdminIndexView.as_view(), name='admin_view'),
 ]
 
--- garderie/views.py
+++ garderie/views.py
@@ -1,9 +1,27 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Child
+from .models import Child, Parent
 from django.urls import reverse
 from django.views import generic
-from django.contrib.auth.models import User
+
+class IndexRedirectView(generic.RedirectView):
+	
+	def get_redirect_url(self, *args, **kwargs):
+		user=self.request.user
+		if user.is_superuser:
+			return reverse('admin_view')
+#		elif user.is_staff:
+#			return reverse('educ_view')
+#		else:
+#			return reverse('parent_view')
+
+class AdminIndexView(generic.ListView):
+	template_name="garderie/admin-index.html"
+	context_object_name='parent_list'
+
+	def get_queryset(self):
+		return Parent.objects.all()
+
 
 class ListChildrenView(generic.ListView):
 	template_name='garderie/index.html'
@@ -12,13 +30,19 @@ class ListChildrenView(generic.ListView):
 	def get_queryset(self):
 		return Child.objects.all()
 
+
+class ChildProfileView(generic.DetailView):
+	model=Child
+	template_name='garderie/child_profile.html'
+
+class ParentProfileView(generic.DetailView):
+	model=Parent
+	template_name='garderie/parent_profile.html'
+
+
+
 #	def get_context_data(self, **kwargs):
 #		context = super().get_context_data(**kwargs)
-#		context['user'] = User
+#		context['child'] = Child.objects.all()
 #		return context
-#                
-
-class DetailView(generic.DetailView):
-	model=Child
-	template_name='garderie/detail.html'
 
