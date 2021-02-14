1e81fbf4da1ade4e3363387c71e91f64a5166331
Clément <cl.magniez@laposte.net>
2021-02-14 14:28:09 +0100

    Fix des paths vers les fichiers CSS et de l'accueil non-identifié

--- garderie/views/redirect.py
+++ garderie/views/redirect.py
@@ -16,36 +16,26 @@ class HomeRedirectView(RedirectView):
 class IndexRedirectView(RedirectView):	
 	def get_redirect_url(self, *args, **kwargs):
 		user=self.request.user
-		if user.is_superuser:
+		if not user.is_authenticated:
+			return reverse('login')		
+		elif user.is_superuser:
 			return reverse('admin_index')
 		elif user.is_staff:
 			return reverse('educ_index')
 		else:
 			return reverse('parent_index')
 
-# Redirection après le login selon le type d'utilisateur
-class IndexRedirectView(RedirectView):	
-	def get_redirect_url(self, *args, **kwargs):
-		user=self.request.user
-		if user.is_superuser:
-			return reverse('admin_index')
-		elif user.is_staff:
-			return reverse('educ_index')
-		else:
-			return reverse('parent_index')
 
 # TODO Voir s'il ne serait pas tout aussi simple de 
 # se débarrasser de EducRedirect et ParentRedirect
 
 # Redirection d'une éducatrice
-class EducRedirectView(RedirectView):
-	
+class EducRedirectView(RedirectView):	
 	def get_redirect_url(self, *args, **kwargs):
 		return reverse('children_list')
 
 # Redirection d'un parent
 class ParentRedirectView(RedirectView):
-	
 	def get_redirect_url(self, *args, **kwargs):
 		user=self.request.user
 		return reverse('parent_profile', args=[user.id])		
